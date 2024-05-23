cbf05cb5b6886ac17390e44bd9e419c7b3519200
Scott Lawrence <srlawrence@lanl.gov>
2024-05-23 16:59:25 -0600

    Exports

--- src/CONCAVE.jl
+++ src/CONCAVE.jl
@@ -1,7 +1,13 @@
 module CONCAVE
 
+export ConvexProgram, SemidefiniteProgram, DenseSDP
+
 include("programs.jl")
 include("algebra.jl")
 include("ipm.jl")
+
+using .Programs
+using .Algebra
+using .IPM
 
 end
