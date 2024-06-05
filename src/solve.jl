a87e5a70b16609530957727fbeaf839dfde53540
Scott Lawrence <srlawrence@lanl.gov>
2024-06-04 18:07:08 -0600

    Constructing demo Hamiltonian

--- src/solve.jl
+++ src/solve.jl
@@ -5,7 +5,17 @@ using CONCAVE
 demo(s::Symbol) = demo(Val(s))
 
 function demo(::Val{:RT})
-    I,c = BosonAlgebra()
+    # Parameters.
+    ω = 1.
+    λ = 0.1
+
+    # Construct operators.
+    I,a = BosonAlgebra()
+    x = sqrt(1/(2*ω)) * (a + a')
+    p = 1im * sqrt(ω/2) * (a' - a)
+
+    # Build the Hamiltonian.
+    H = p^2 / 2 + ω^2 * x^2 / 2 + λ * x^4 / 4
 end
 
 function main()
