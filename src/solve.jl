00cce8da741f217dd6cbac9e3aaae5f5ab740d2f
Scott Lawrence <srlawrence@lanl.gov>
2025-09-07 17:45:30 -0600

    Hamiltonian

--- src/solve.jl
+++ src/solve.jl
@@ -13,18 +13,124 @@ import CONCAVE.Programs: initial, constraints!, objective!
 demo(s::Symbol; verbose=false) = demo(Val(s), verbose)
 
 struct NeutronMatterProgram <: ConvexProgram
+    L::Int
+    g1::Float64
+    g2::Float64
+    μ::Float64
+    a::Float64
+
+    function NeutronMatterProgram(L::Int, g1::Float64, g2::Float64, μ::Float64, a::Float64; periodic=true)
+        V = L^3
+
+        function index(x,y,z)
+            return (x-1)*L*L + (y-1)*L + z
+        end
+
+        # Construct algebra
+        @algebra NeutronAlgebra begin
+            I::Identity
+            cu::Dirac[V]
+            cd::Dirac[V]
+        end
+
+        # Construct Hamiltonian
+        H = 0*I
+        # Hopping, and neighbor interaction
+        function hopping(i,j)
+            hu = adjoint(cu[i])*cu[j] + adjoint(cu[j]) * cu[i]
+            hd = adjoint(cd[i])*cd[j] + adjoint(cd[j]) * cd[i]
+            # TODO coefficient
+            return hu + hd
+        end
+        function nninteraction(i,j)
+            ni = adjoint(cu[i])*cu[i] + adjoint(cd[i])*cd[i]
+            nj = adjoint(cu[j])*cu[j] + adjoint(cd[j])*cd[j]
+            # TODO coefficient
+            return ni*nj
+        end
+        for x in 1:L, y in 1:L, z in 1:L
+            xp = mod1(x+1,L)
+            yp = mod1(y+1,L)
+            zp = mod1(z+1,L)
+            wrapx = xp < x
+            wrapy = yp < y
+            wrapz = zp < z
+            i = index(x,y,z)
+            if !wrapx || periodic
+                j = index(xp,y,z)
+                H += hopping(i,j)
+                H += nninteraction(i,j)
+            end
+            if !wrapy || periodic
+                j = index(x,yp,z)
+                H += hopping(i,j)
+                H += nninteraction(i,j)
+            end
+            if !wrapz || periodic
+                j = index(x,y,zp)
+                H += hopping(i,j)
+                H += nninteraction(i,j)
+            end
+        end
+        # Same-site interaction.
+        for x in 1:L, y in 1:L, z in 1:L
+            i = index(x,y,z)
+            nu = adjoint(cu[i])*cu[i]
+            nd = adjoint(cd[i])*cd[i]
+            # TODO lattice spacing
+            H += g1 * nu * nd
+        end
+
+        # Chemical potential
+        N = 0*I
+        for x in 1:L, y in 1:L, z in 1:L
+            i = index(x,y,z)
+            N += adjoint(cu[i])*cu[i]
+            N += adjoint(cd[i])*cd[i]
+        end
+        H -= μ*N
+
+        # Construct the psd matrices.
+        M = [] # TODO
+
+        # Identify constraint matrices.
+        # TODO
+        
+        # Construct objective matrix.
+        C = 0 # TODO
+
+        new(L,g1,g2,μ,a,A,b,C)
+    end
+end
+
+function size(p::NeutronMatterProgram)::Int
+    # TODO
+    return 1
+end
+
+function initial(p::NeutronMatterProgram)::Vector{Float64}
+    return rand(Float64, size(p))
+end
+
+function objective!(g, p::NeutronMatterProgram, y::Vector{Float64})::Float64
+    # TODO
+    return 0.0
+end
+
+function constraints!(cb, p::NeutronMatterProgram, y::Vector{Float64})
+    # TODO
+    # cb(Λ, dΛ, hessian). We expect the hessian to be 0.
 end
 
 function demo(::Val{:NeutronMatter}, verbose::Bool)
     L::Int = 4
-    V = L^3
-    @algebra NeutronAlgebra begin
-        c::Dirac[V]
-    end
+    g1::Float64 = 0.1
+    g2::Float64 = 0.1
+    a::Float64 = 0.5
 
     for μ in 0:0.1:1.0
-        plo = NeutronMatterProgram()
-        phi = NeutronMatterProgram()
+        plo = NeutronMatterProgram(L, g1, g2, μ, a)
+        phi = NeutronMatterProgram(L, g1, g2, μ, a)
 
         lo, ylo = CONCAVE.IPM.solve(plo; verbose=verbose)
         hi, yhi = CONCAVE.IPM.solve(phi; verbose=verbose)
