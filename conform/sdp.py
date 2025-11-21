Hessian of barrier is correct

conform/sdp.py
@@ -156,7 +156,9 @@ class _Phase1Program:
             g[0] = np.trace(Minv).real
             h = np.zeros((self.K,self.K))
             h[1:,1:] = h_.real
-            # TODO differentiate
+            h[0,0] = -np.einsum("ij,ji", Minv, Minv).real
+            h[0,1:] = -np.einsum("ij,ajk,ki->a", Minv, self.sdp.m, Minv).real
+            h[1:,0] = h[0,1:]
             return -ld, -g, -h
         return -ld
 

conform/test_sdp.py
@@ -90,7 +90,9 @@ def test_phase1_barrier_hessian():
         for k in range(K):
             yk = y.copy()
             yk[k] += eps
+            _, gp, _ = phase1.barrier(yk, differentiate=True)
             yk[k] -= 2*eps
-            d = 0
-            assert np.abs(d) < 2e-4
+            _, gm, _ = phase1.barrier(yk, differentiate=True)
+            d = (gp-gm)/(2*eps)
+            assert np.mean(np.abs(d - h[:,k])) < 1e-3
 